d86e8cbc4ada3633dff89512ced0646743e5073f
hariprasath <hariprasathk38@gmail.com>
2025-10-31 22:21:54 +0530

    Tester 1

--- src/test/java/com/abc/phyton/Language.java
+++ src/test/java/com/abc/phyton/Language.java
@@ -1,5 +1,18 @@
 package com.abc.phyton;
 
 public class Language {
+	
+	
+  private void animal() {
+System.out.println("Deer");
+	  
+	  
+}
+  
+  private void bird() {
+	  
+	  System.out.println("parrot");
+
+}
 
 }
