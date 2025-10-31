dc5252c434b2cb9ee4b27ddf0937a5893e1b173a
hariprasath <hariprasathk38@gmail.com>
2025-10-31 22:35:14 +0530

    Tester 2 commit

--- src/test/java/com/abc/phyton/Language.java
+++ src/test/java/com/abc/phyton/Language.java
@@ -1,5 +1,19 @@
 package com.abc.phyton;
 
 public class Language {
+	
+	
+	private void india() {
+
+		System.out.println("India is my country");
+		
+	}
+	
+	
+	private void Usa() {
+System.out.println("United states of america");
+		
+	}
+	
 
 }
